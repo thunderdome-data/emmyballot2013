08af69e39cb016914d85bdef3dc1d34a093c38b5
freejoe76 <jmurphy@denverpost.com>
2015-09-16 14:01:49 -0400

    We're going to be moving a lot of global functions into config.php

--- config.php
+++ config.php
@@ -1,4 +1,31 @@
 <?php
 // This makes it easier to repurpose this app.
-$current_year = 2015;
+$current_year = '2015';
+$data_source = 'csv'; // Either 'csv' or 'db'
 
+$conf = array(
+    '2015' => array(
+        'intro' => '',
+        )
+);
+
+function csv_to_array($filename='', $delimiter=',')
+{
+    if(!file_exists($filename) || !is_readable($filename))
+        return FALSE;
+
+    $header = NULL;
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+        while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+        {
+            if(!$header)
+                $header = $row;
+            else
+                $data[] = array_combine($header, $row);
+        }
+        fclose($handle);
+    }
+    return $data;
+}
